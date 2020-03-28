87f6542f3b2184ea768a39a2ddd45b8964abf63b
wei.zhi <411552711@qq.com>
2020-03-28 17:50:20 +0800

    1

--- variableAndOperate.py
+++ variableAndOperate.py
@@ -2,43 +2,59 @@
 
 # -*-coding:utf-8-*-
 
-# python 单行注释
-'''
-python 多行注释
-'''
+# a = 5
+# print(type(a))  # type()输出传入数据的类型
+# a = "hello "
+# print(type(a))
 
+# print函数
+# name = "weizhi"
+# age = 20
+# print("姓名", name ,"性别",age,sep="|")  # 改变默认的分隔符
+# print("姓名", name ,"性别",age)
 
-# pyhton变量命名必须是大小写的英文字母、数字、下划线（_）的组合，不能以数字开头
-# python的变量类型及其运算符
-# 变量：“盛装”程序中的数据，变量保存的数据可以多次发生改变，只要程序对变量重新赋值即可
-# 常量：常量保存的数据不可以发生改变
+# print(40,"\t",end="")  # print默认换行（end="\n"），输出不换行修改end的值
+# print(50,"\t",end="")
+# print(60,"\t",end="\n")
 
+# f = open("1.txt",'r+',encoding="utf-8")
+# print("海上生明月",file = f)
+# print("天涯共此时",file = f)
+# print(f.read())
+# f.close()
 
-'''
-# 强类型语言：C，C++，java
-# 弱类型语言 ：python
-弱类型语言的特征：
-（1）变量无须声明即可赋值，对一个不存在的变量赋值就相当于定义了一个新的变量
-（2）变量的数据类型可以动态改变：同一个变量可以一会儿被赋值为整数值，一会儿被赋值为字符串
-a = 5
-'''
-a = 5
-print(type(a))  # ype()输出传入数据的类型
-a = "hello "
-print(type(a))
+#复数
+# s = 3 + 1j
+# print(type(s)) # int 
 
-# 数值类型 不可变 python修改数值类型变量的值，其实只是修改变量名所表示的内存空间
-# 数值类型： 整形、 浮点型、 复数类型
-'''
-整形：正整数、负整数、0
-java： 根据数值大小 选择 short、int、long
-python中整形支持存储各种整数值，无论多大或者多小，pyhton都能轻松处理
-python的整形支持None值
-        a = None 
-        print(a)  #输出None
-python3.X支持数值（包括浮点型）增加下划线作为分隔符，这些下划线不会影响数值本身的大小
-'''
-name = "weizhi"
-age = 20
-print("姓名", name ,"性别",age,sep="|")
-print("姓名", name ,"性别",age)
+# msg = input("请输入一个值：")
+# print(type(msg))
+# # print(msg)
+
+# s1 = ["helo",90,"world"]
+# s2 = ["1","20",30]
+# print(s1 + s2)
+
+# a_range = range(5)
+# print(list(a_range)) # list()转换为列表
+# print(tuple(a_range)) #tuple()转换为元组
+
+# c_list = list(range(1,5))
+# print(c_list)
+# c_list.insert(3,"hello")
+# print(c_list)
+# c_list.insert(3,tuple("hello"))
+# print(c_list)
+# a = "hello"
+# print(tuple(a))
+# print(list(a))
+
+# b_list = list(range(1,10))
+# print(b_list)
+# del b_list[2:-2:2]
+# print(b_list)
+# del b_list[2:4]
+# print(b_list)
+
+d_dict = {(20,30):'good',30:'bad'}
+print(d_dict)
